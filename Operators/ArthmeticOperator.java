849284fdf5a19f940c4700c09ffac4e6908e9009
Ayush Singh <singhayush886@gmail.com>
2023-12-15 11:01:05 +0530

    DSA

--- Operators/ArthmeticOperator.java
+++ Operators/ArthmeticOperator.java
@@ -6,13 +6,56 @@ public class ArthmeticOperator {
     public static void main(String[] args) {
         // Arthmetic Operator
         // +, * , /, % ,-
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = a + b;
-        System.out.println("Ans = " + c);
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = a + b;
+        // System.out.println("Ans = " + c);
 
         // Assignment Operator----> (=)
         // +=, -=,*=,/=,%=
+
+        int c = 4;
+        int d = 7;
+        System.out.println(c += d);
+        System.out.println(c -= d);
+        System.out.println(c *= d);
+        System.out.println(c /= d);
+        System.out.println(c %= d);
+
+        // Output
+
+        /*
+         * 11
+         * 4
+         * 28
+         * 4
+         * 4
+         */
+
+        // Relational Operator
+        int firstNumber = 12;
+        int secondNumber = 16;
+        System.out.println(firstNumber == secondNumber);
+        System.out.println(firstNumber != secondNumber);
+        System.out.println(firstNumber > secondNumber);
+        System.out.println(firstNumber < secondNumber);
+
+        System.out.println(firstNumber >= secondNumber);
+        System.out.println(firstNumber <= secondNumber);
+        // output
+
+        /*
+         * false
+         * true
+         * false
+         * true
+         * false
+         * true
+         */
+
+        // Logical Operator
+        // &&, ||,!
+
     }
 }
